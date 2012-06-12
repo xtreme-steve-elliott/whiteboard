4e74f8fa0a22fb5c238c9852ae9a7948f6dc9ce4
mkocher <mkocher@pivotallabs.com>
2012-06-11 23:40:15 -0700

    Tooltips on mobile.
    qTip2 really should support this natively

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,17 @@ jQuery(function($) {
     $('.has-qtip').qtip({
         position: {
             viewport: $(window),
+            my: 'top middle',
             at: 'bottom middle'
         },
         style: {
             classes: 'ui-tooltip-bootstrap'
+        },
+        events: {
+            render: function(event, api) {
+                api.elements.target.click(api.toggle);
+                api.elements.tooltip.click(api.hide);
+            }
         }
     });
 
