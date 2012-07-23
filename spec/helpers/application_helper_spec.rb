403387e5c024d5abc2cd99964d2562587a74c193
Will Read <will.read@gmail.com>
2012-07-22 21:52:55 -0700

    Fixing broken build...
    ... which is caused by internal disagreement on how to end the
    standup. No one stretches. Let it go people. Just let it go.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,7 +4,7 @@ describe ApplicationHelper do
   describe "#close_standup" do
     it "picks a closing" do
       helper.should_receive(:rand).and_return(0)
-      helper.standup_closing.should == "And That's The Standup"
+      helper.standup_closing.should == "STRETCH!"
     end
   end
 
