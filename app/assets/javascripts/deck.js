7469ada89a64e7e1113ce7950ef4fec42baf829b
Steve Ellis <email@steveell.is>
2012-09-23 19:25:51 -0400

    Add item descriptions to presentation mode

--- app/assets/javascripts/deck.js
+++ app/assets/javascripts/deck.js
@@ -2,3 +2,23 @@
 //= require modernizr.custom
 //= require core/deck.core
 //= require extensions/hash/deck.hash
+//= require jquery.qtip.min
+
+jQuery(function($) {
+    $('.has-qtip').qtip({
+        position: {
+            viewport: $(window),
+            my: 'top middle',
+            at: 'bottom middle'
+        },
+        style: {
+            classes: 'ui-tooltip-bootstrap'
+        },
+        events: {
+            render: function(event, api) {
+                api.elements.target.click(api.toggle);
+                api.elements.tooltip.click(api.hide);
+            }
+        }
+    });
+});
