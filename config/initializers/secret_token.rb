d73c16cca8fc804e28809df5b98e8fd70ce0db2c
John Foley <john@hisfoleyness.com>
2013-07-09 09:27:06 -0600

    Silence warning about secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Whiteboard::Application.config.secret_token = 'e9477bca0ebf7ec97a2706d48bd3c19abbb34f9866792cce24c7589d2951f88f2de81433b5e3a29152243c25ef5de97799dad21de30e67da15b96faa89508b76'
+Whiteboard::Application.config.secret_key_base = 'e9477bca0ebf7ec97a2706d48bd3c19abbb34f9866792cce24c7589d2951f88f2de81433b5e3a29152243c25ef5de97799dad21de30e67da15b96faa89508b76'
