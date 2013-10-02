5a6150febbc27fc592ac1bde5c7aa8b31db91529
Brian Buchalter and Elaine Tai <pair+bbuchalter+etai@pivotallabs.com>
2013-10-02 13:22:55 -0600

    Update mail settings to use CF sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,18 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
-}
+require 'json'
+
+if ENV['VCAP_SERVICES'].present?
+
+  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+  # The namespaces for vcap services are defined in the manifest.yml
+  credentials = vcap_services["sendgrid-n/a"][0]['credentials']
+
+  ActionMailer::Base.smtp_settings = {
+    :address => credentials['hostname'],
+    :port => '587',
+    :authentication => :plain,
+    :user_name => credentials['username'],
+    :password => credentials['password'],
+    :domain => 'cfapps.io'
+  }
+
+end
