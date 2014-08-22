f351db8d58c0987802fe6673d8826880f92d2fcf
Charlie Bevis and Greg Cobb <pair+cbevis+gcobb@pivotallabs.com>
2014-08-22 10:31:45 -0700

    [#77392638] Increases Capy wait time and updates Travis script

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'capybara/rails'
 require 'capybara/rspec'
 Capybara.javascript_driver = :selenium
 Capybara.default_driver = :webkit
+Capybara.default_wait_time = 5
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
